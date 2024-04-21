372a2cbc999a9dec7e5ecbb2311fdd17bffcb1bc
= <=>
2024-04-21 05:04:24 -0700

    first commit

---
+++ ios-money-management/ios-money-management/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-money-management
+//
+//  Created by nguyenthanhnhan on 02/02/1403 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
