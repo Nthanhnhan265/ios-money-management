86c136726f42f10c432a2009f7911665e1f3d1d4
ngTamAn111103 <ng.t.ann2003@gmail.com>
2024-04-22 18:26:49 +0700

    Forgot Password_Email

--- ios-money-management/ios-money-management/Controllers/ForgotPasswordViewController.swift
+++ ios-money-management/ios-money-management/Controllers/ForgotPasswordViewController.swift
@@ -6,24 +6,41 @@
 //
 
 import UIKit
+import Firebase
 
 class ForgotPasswordViewController: UIViewController {
 
+    @IBOutlet weak var btn_Continue: UIButton!
+    @IBOutlet weak var btn_SentOTP: UIButton!
+    @IBOutlet weak var txt_otp: UITextField!
+    @IBOutlet weak var txt_email: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        
+        print("Vào ForgotPasswordViewController")
+        
+        self.navigationItem.title = "Forgot Password"
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func btn_SentOTP_tapped(_ sender: UIButton) {
+        
+        
+        guard let email = txt_email.text else{return}
+        Auth.auth().sendPasswordReset(withEmail: email) { (error) in
+            if error == nil {
+                print("SEND OTP")
+            }
+            else
+            {
+                guard let error = error else{return}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+                print(error)
+            }
+        }
+        
+        
     }
-    */
-
+    
 }
