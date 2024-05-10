26075f7eafa44cac070932b276f393763882ba0e
ngTamAn111103 <ng.t.ann2003@gmail.com>
2024-05-10 14:54:18 +0700

    Hoàn thành giao diện fe

--- ios-money-management/ios-money-management/Controllers/DetailWalletViewController.swift
+++ ios-money-management/ios-money-management/Controllers/DetailWalletViewController.swift
@@ -1,24 +1,18 @@
 //
-//  TransactionViewController.swift
+//  DetailWalletViewController.swift
 //  ios-money-management
 //
-//  Created by AnNguyen on 06/05/2024.
+//  Created by AnNguyen on 10/05/2024.
 //
 
 import UIKit
 
-class TransactionViewController: UIViewController {
-    struct Section {
-        let date: Date
-        let transactions: [Transaction]
-    }
-    @IBOutlet weak var tableview: UITableView!
-    //    Dữ liệu giả
-    @IBOutlet weak var popup_time: UIButton!
-    @IBOutlet weak var view_rangeTime: UIView!
-    @IBOutlet weak var popup_cate: UIButton!
-    @IBOutlet weak var view_filter: UIView!
+class DetailWalletViewController: UIViewController {
+
+//    Dữ liệu giả
     private var datas = [Transaction]()
+    @IBOutlet weak var tableview: UITableView!
+
     var sections: [Section] = [
         Section(date: Date(), transactions: [
             Transaction(name: "Shopping", img: UIImage(named: "Frame1"), balance: 120000, time: Date(), des: "123"),
@@ -29,47 +23,53 @@ class TransactionViewController: UIViewController {
             Transaction(name: "Shopping", img: UIImage(named: "Frame1"), balance: 120000, time: Date().addingTimeInterval(-24 * 60 * 60), des: ""),
         ]),
     ]
-
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Vào TransactionViewController")
+
+        print("Vào DetailWalletViewController")
         
+//        set dữ liệu cho table
         //        Setting cho table view
         tableview.dataSource = self
         tableview.delegate = self
         tableview.register(TransactionTableViewCell.nib(), forCellReuseIdentifier: TransactionTableViewCell.identifier)
         
+        
         setDataTransaction()
-        setCategory()
-        setTimeline()
-    }
-    func setTimeline()  {
-        let optionClosure = { (action: UIAction) in
-             print(action.title)
-           }
-
-        popup_time.menu = UIMenu(children: [
-             UIAction(title: "Tháng", state: .on, handler: optionClosure),
-             UIAction(title: "Ngày", handler: optionClosure),
-             UIAction(title: "Năm", handler: optionClosure),
-           ])
     }
     
-    @IBAction func btn_rangeTime_Tapped(_ sender: UIBarButtonItem) {
-        view_rangeTime.isHidden = !view_rangeTime.isHidden
-        
-    }
-    func setCategory()  {
-        let optionClosure = { (action: UIAction) in
-             print(action.title)
-           }
 
-        popup_cate.menu = UIMenu(children: [
-             UIAction(title: "Tổng cộng", state: .on, handler: optionClosure),
-             UIAction(title: "MB Bank", handler: optionClosure),
-             UIAction(title: "Tiền mặt", handler: optionClosure),
-           ])
+
+}
+extension DetailWalletViewController: UITableViewDataSource, UITableViewDelegate{
+//    UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return datas.count
+        return sections[section].transactions.count
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableview.dequeueReusableCell(withIdentifier: TransactionTableViewCell.identifier, for: indexPath) as! TransactionTableViewCell
+        let item = datas[indexPath.row]
+
+//Bỏ thông tin vào các UI của cell
+        cell.transaction_name.text = item.transactionName
+        cell.transaction_img.image = item.transactionImage
+        cell.transaction_description.text = item.transactionDes
+        cell.transaction_balance.text = String(-12345)
+        cell.transaction_time.text = DateToString(item.transactionTime!)
+            
+        return cell
+    }
+//    Hàm set title TODAY, YESTERDAY...
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "dd/MM/yyyy"
+            return dateFormatter.string(from: sections[section].date)
     }
     /// Hàm chuyển đồ từ Date sang String
     func DateToString(_ date:Date) -> String{
@@ -115,54 +115,16 @@ class TransactionViewController: UIViewController {
             return Date.now
         }
     }
-//    Hàm set dữ liệu giả cho ví
-    func setDataTransaction() {
-        for i in 10..<30{
-            if let time = StringToDate("\(i)/05/2024"){
-                datas.append( Transaction(name: "Shopping", img: UIImage(named: "Frame1"), balance: 120000, time: time, des: ""))
+    //    Hàm set dữ liệu giả cho ví
+        func setDataTransaction() {
+            for i in 10..<30{
+                if let time = StringToDate("\(i)/05/2024"){
+                    datas.append( Transaction(name: "Shopping", img: UIImage(named: "Frame1"), balance: 120000, time: time, des: ""))
+                }
             }
-        }
-        
-        
-    }
-
-    @IBAction func btn_filter_tapped(_ sender: UIBarButtonItem) {
-        view_filter.isHidden = !view_filter.isHidden
-    }
-    
-    
-    
-}
-extension TransactionViewController: UITableViewDataSource, UITableViewDelegate{
-//    UITableViewDataSource
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return datas.count
-        return sections[section].transactions.count
-    }
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return sections.count
-    }
-
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableview.dequeueReusableCell(withIdentifier: TransactionTableViewCell.identifier, for: indexPath) as! TransactionTableViewCell
-        let item = datas[indexPath.row]
-
-//Bỏ thông tin vào các UI của cell
-        cell.transaction_name.text = item.transactionName
-        cell.transaction_img.image = item.transactionImage
-        cell.transaction_description.text = item.transactionDes
-        cell.transaction_balance.text = String(-12345)
-        cell.transaction_time.text = DateToString(item.transactionTime!)
             
-        return cell
-    }
-//    Hàm set title TODAY, YESTERDAY...
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        let dateFormatter = DateFormatter()
-            dateFormatter.dateFormat = "dd/MM/yyyy"
-            return dateFormatter.string(from: sections[section].date)
-    }
+            
+        }
 
     
     //  UITableViewDelegate
@@ -181,4 +143,7 @@ extension TransactionViewController: UITableViewDataSource, UITableViewDelegate{
         navigationController?.pushViewController(view_controller, animated: true)
     }
 }
-
+struct Section {
+    let date: Date
+    let transactions: [Transaction]
+}
