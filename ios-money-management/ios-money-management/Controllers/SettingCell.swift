0472b18922258edad9c35ed944bd18a0b3b7f73f
Nthanhnhan265 <=>
2024-05-10 07:50:34 -0700

    done: ui edit profile, ui profile

---
+++ ios-money-management/ios-money-management/Controllers/SettingCell.swift
@@ -0,0 +1,14 @@
+//
+//  SettingCell.swift
+//  ios-money-management
+//
+//  Created by nguyenthanhnhan on 21/02/1403 AP.
+//
+
+import UIKit
+
+class SettingCell: UITableViewCell {
+    @IBOutlet weak var settingImage: UIImageView!
+    @IBOutlet weak var settingName: UILabel!
+    
+}
